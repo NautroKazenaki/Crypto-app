fa23e1ad7d2c214438f2b0a1a58b561a25966501
Сергей Ашарин <kalliak2014@yandex.ru>
2022-01-27 21:07:56 +0300

    building navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
+import {Layout, Typography, Space} from 'antd'
+
+import {Navbar} from './components'
+import './App.css'
 
 const App = () =>{
     return (
-        <div>
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
 
+            </div>
+            <div className="footer">
+
+            </div>
         </div>
     )
 }
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+//to import all components in 1 line
+export  {default as Navbar} from './Navbar'
