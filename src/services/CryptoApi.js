e4a7255c0e3487b6df9e2e2c711e15b19893a9e6
Сергей Ашарин <kalliak2014@yandex.ru>
2022-01-27 22:44:58 +0300

    using custom Api

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -1,10 +1,10 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': 'd043d3f67bmsh5ab7e98d12fabc3p1b500bjsna5228342596f'
 }
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
 const createRequest = (url) => ({url, headers: cryptoApiHeaders})
 
@@ -13,7 +13,13 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({baseUrl}),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/exchanges')
+            query: () => createRequest('/coins')
         })
     })
 })
+
+export const {
+    //redux toolkit hook to instantly get data from query
+    //!!!! 'use' at the start and 'Query' in the end
+    useGetCryptosQuery,
+} = cryptoApi;
