a79dd671bcf50d81843e9c36d71dcb334d72534c
Сергей Ашарин <kalliak2014@yandex.ru>
2022-01-27 23:11:26 +0300

    homepage component + cryptocurrencies component setuped

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import millify from 'millify'
+import {Link} from 'react-router-dom'
+import {Card, Row, Col, Input} from 'antd'
+
+import {useGetCryptosQuery} from '../services/CryptoApi'
 
 const Cryptocurrencies = () => {
+    //to get coins list
+    const {data : cryptosList, isFetching} = useGetCryptosQuery()
+    //to get access to all the coins
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+    /* 
+        1. React component
+        2. Row
+        3. Columns styled for each screen size
+        4. Link which route to selected crypto coin
+        5. Card w/ info 'bout crypto coin
+    */
     return (
-        <div>
-            Cryptocurrencies
-        </div>
+        <>
+            {/* gutters = spaces between the items */}
+            <Row gutters = {[32,32]} className="crypto-card-container">
+                {cryptos.map( (currency) => (
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+                        <Link to={`/crypto/${currency.id}`}>
+                            <Card 
+                                title={`${currency.rank}. ${currency.name}`}
+                                extra={<img className="crypto-image" src={currency.iconUrl}/>}
+                                hoverable
+                            >
+                                <p> Price: {millify(currency.price)}</p>
+                                <p> Market Cap: {millify(currency.marketCap)}</p>
+                                <p> Daily Change: {millify(currency.change)}%</p>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,8 +4,9 @@ import {Typography, Row, Col, Statistic} from 'antd'
 import {Link} from 'react-router-dom'
 
 import {useGetCryptosQuery} from '../services/CryptoApi'
-//short version of Typography.Title
+import {Cryptocurrencies, News } from '../components'
 
+//short version of Typography.Title
 const {Title} = Typography
 
 const Homepage = () => {
@@ -22,6 +23,16 @@ const Homepage = () => {
                 <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
                 <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
             </Row>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+            </div>
+            <Cryptocurrencies simplified />
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show more</Link></Title>
+            </div>
+            <News simplified />
         </>
     )
 }
