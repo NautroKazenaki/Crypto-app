4f67f672f77678a573aaf06f17067792341cae79
Сергей Ашарин <kalliak2014@yandex.ru>
2022-01-28 18:21:07 +0300

    evolve w/ input which can selected a specific Crypto news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Select, Typography, Row, Col, Avatar, Card} from 'antd'
 import moment from 'moment'
 
 import {useGetCryptoNewsQuery} from '../services/CryptoNewsApi'
+import {useGetCryptosQuery} from '../services/CryptoApi'
 
 const {Text, Title} = Typography
 const {Option} = Select
@@ -27,18 +28,35 @@ const demoImage = "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=Ne
     wew, xd
 */
 const News = ({simplified}) => {
-    const {data: cryptoNews} = useGetCryptoNewsQuery({newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12})
-
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
+    const {data: cryptoNews} = useGetCryptoNewsQuery({newsCategory, count: simplified ? 6 : 12})
+    const {data} = useGetCryptosQuery(100);
     if (!cryptoNews?.value) return 'Loading...'
     return (
         <Row gutter={[24,24]}>
+            {!simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="selevt-news"
+                        placeholder="Select a Crypto"
+                        optionFilterProp="children"
+                        onChange={(value) => setNewsCategory(value)}
+                        //filter the options
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    >
+                        <Option value="Cryptocurrency"> Cryptocurrency </Option>
+                        {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
+                    </Select>
+                </Col>
+            )}
             {cryptoNews.value.map( (news, i) => (
                 <Col xs={24} sm={12} lg={8} key={i}>
                     <Card hoverable className="news-card">
                         <a href={news.url} target="_blank" rel="noreferrer">
                             <div className="news-image-container">
                                 <Title className="news-title" level={4}> {news.name} </Title>
-                                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                                <img style={{maxWidth: '200px', maxHeight: '100px'}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
                             </div>
                             <p>
                                 {news.description > 100 ? `${news.description.substring(0, 100)} ...`
